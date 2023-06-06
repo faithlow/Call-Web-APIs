20551f48763b6ab7c5de8d730b79d63a1a5f2627
loolooyu <133681498+loolooyu@users.noreply.github.com>
2023-06-06 15:46:16 +0000

    Define get exchange rate

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-oldcurrency = input("What currency do you want to exchange?")
-newcurrency = input("What currency do you want to exchange it to?")
+base_currency = "USD" #base currency must be USD because it is included in the URL
+target_currency = input("What currency do you want to exchange it to? ").upper()
+
+import requests 
+
+def get_exchange_rate(base_currency, target_currency):
+  api_key = b737cf750c85f10fd181f880 #personal api key
+  URL = "https://v6.exchangerate-api.com/v6/b737cf750c85f10fd181f880/latest/USD" #api key within the URL
+  response = requests.get(URL)
+  data = response.json()
