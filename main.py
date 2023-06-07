835e80a5a62357c59c910261a9d6ccbf75d03930
faithlow <133681603+faithlow@users.noreply.github.com>
2023-06-07 06:21:55 +0000

    defined call api section

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+import requests 
 
+def get_exchange_rate(base_currency, target_currency):
+  api_key = "b737cf750c85f10fd181f880" #personal api key
+  URL = "https://v6.exchangerate-api.com/v6/" + api_key + "/latest/" + base_currency #api key within the URL
+  response = requests.get(URL)
+  
+  data = response.json()
+  if response.status_code == 200:
+      if target_currency in data["conversion_rates"]:
+        exchange_rate = data["conversion_rates"][target_currency]
+        return exchange_rate
+      else:
+        return None
+  else:
+      return None
+    
 
 if __name__ == '__main__': #start of program execution
   user_base_currency = input("What currency do you want to convert from: ").upper() #base currency must be USD because it is included in the URL
